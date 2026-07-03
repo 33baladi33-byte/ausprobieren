467542c95908c44365da2a4697855e8f1ff511d0
33baladi33-byte <33.baladi.33@gmail.com>
2026-07-03 21:01:13 +0100

    Update highlightData.js

--- highlightData.js
+++ highlightData.js
@@ -9,6 +9,28 @@ const HIGHLIGHT_DATA = {
         q3: { text: "Pflegeheimen", color: 3 },
         q4: { text: "Heizkosten", color: 4 },
         q5: { text: "Dopingsperre", color: 5 }
+    },
+    "hoeren1_exam2": {
+        q1: { text: "Piloten", color: 1 }
+    },
+    "hoeren2_exam1": {
+        q1: { text: "Herr Gasser", color: 1 },
+        q2: { text: "Frau Janke", color: 2 }
+    },
+    "lesen1_exam1": {
+        q1: { text: "Jugend Forscher", color: 1 }
+    },
+    "lesen2_exam1": {
+        q1: { text: "Krista", color: 1 }
+    },
+    "lesen3_exam1": {
+        q1: { text: "Filme", color: 1 }
+    },
+    "sprach1_exam1": {
+        q1: { text: "Hallo Ferdinand", color: 1 }
+    },
+    "sprach2_exam1": {
+        q1: { text: "Das Fahrrad", color: 1 }
     }
 };
 
