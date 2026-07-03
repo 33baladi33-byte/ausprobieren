71b0ad9278b6be3c3cc473f1112de5fb80f40981
33baladi33-byte <33.baladi.33@gmail.com>
2026-07-03 20:47:07 +0100

    Update highlightData.js

--- highlightData.js
+++ highlightData.js
@@ -1,24 +1,9 @@
 const HIGHLIGHT_DATA = {
     "hoeren1_exam1": {
-        q1: {
-            text: "Die Deutsche Lufthansa hat ihre Führungsposition verloren.",
-            color: 1
-        },
-        q2: {
-            text: "Ein Bonner Sportverein zeigt der Öffentlichkeit sein Angebot.",
-            color: 2
-        },
-        q3: {
-            text: "Die Rahmenbedingungen in deutschen Pflegeheimen bedürfen dringend einer Änderung.",
-            color: 3
-        },
-        q4: {
-            text: "Durch moderne Wärmedämmung könnten die Heizkosten deutlich reduziert werden.",
-            color: 4
-        },
-        q5: {
-            text: "Das Nationale Olympische Komitee hat die Dopingsperre für Tom Sanders aufgehoben.",
-            color: 5
-        }
+        q1: { text: "Lufthansa", color: 1 },
+        q2: { text: "Sportverein", color: 2 },
+        q3: { text: "Pflegeheimen", color: 3 },
+        q4: { text: "Heizkosten", color: 4 },
+        q5: { text: "Dopingsperre", color: 5 }
     }
 };
