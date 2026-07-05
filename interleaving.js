2d384cc42cd25f801d9f6adade57d9b9a993c45d
33baladi33-byte <33.baladi.33@gmail.com>
2026-07-05 12:03:44 +0100

    Update interleaving.js

--- interleaving.js
+++ interleaving.js
@@ -1,4 +1,4 @@
-// interleaving.js - إدارة خلط الأسئلة في واجهة العرض فقط
+// interleaving.js - إدارة خلط الأسئلة
 
 class InterleavingManager {
     constructor() {
@@ -7,7 +7,6 @@ class InterleavingManager {
         this.originalIndices = [];
     }
 
-    // خلط مصفوفة باستخدام خوارزمية Fisher-Yates
     shuffleArray(array) {
         const shuffled = [...array];
         for (let i = shuffled.length - 1; i > 0; i--) {
@@ -17,24 +16,19 @@ class InterleavingManager {
         return shuffled;
     }
 
-    // تطبيق الخلط على الأسئلة
     shuffleQuestions(questions) {
         if (!questions || questions.length === 0) return null;
         
-        // حفظ الترتيب الأصلي
         if (!this.isActive) {
             this.originalIndices = questions.map((_, i) => i);
         }
         
-        // إنشاء ترتيب عشوائي جديد
         this.shuffledIndices = this.shuffleArray(questions.map((_, i) => i));
         this.isActive = true;
         
-        // إرجاع الأسئلة بالترتيب الجديد
         return this.shuffledIndices.map(i => questions[i]);
     }
 
-    // إلغاء الخلط والعودة للترتيب الأصلي
     unshuffleQuestions(questions) {
         if (!this.originalIndices.length) return questions;
         
@@ -45,12 +39,6 @@ class InterleavingManager {
         return originalQuestions;
     }
 
-    // الحصول على الترتيب الحالي
-    getCurrentOrder() {
-        return this.isActive ? this.shuffledIndices : this.originalIndices;
-    }
-
-    // إعادة تعيين
     reset() {
         this.isActive = false;
         this.shuffledIndices = [];
